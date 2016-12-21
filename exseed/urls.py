5273844e982909757a5bc1f2b366c1de6c7b912e
Raphael 'Soviet Hammer'  DeFranco <raphd@vadio.com>
2016-12-20 16:11:03 -0800

    Changing a couple of the names.

--- exseed/urls.py
+++ exseed/urls.py
@@ -1,4 +1,4 @@
-"""Waggle URL Configuration
+"""exseed URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
